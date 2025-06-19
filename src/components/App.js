7bbdce19f887b340e315d5269e6cddc3c5cf0346
Tomas Pazout <tom.pazout@gmail.com>
2025-06-19 23:30:08 +0200

    Navbar

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+import Navigation from './Navigation';
+
+function App() {
+    return(
+        <Container>
+            <Navigation />
+        </Container>
+
+    )
+}
+
+export default App;
